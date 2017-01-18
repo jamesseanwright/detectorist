8cef8bdd06ae40e9a8b6ed1294439e18f928ea04
James Wright <JAWright@Channel4.co.uk>
2017-01-18 16:22:26 +0000

    More functional tests

--- test/functionalTests.js
+++ test/functionalTests.js
@@ -4,7 +4,7 @@ const featureConfig = require('./featureConfig.json');
 const isBrowserSupported = require('../');
 
 /* these tests verify the library's behaviour against
- * the sample featureConfig file. Seperate tests are to be
+ * the sample featureConfig file. Seperate E2E tests are to be
  * written for actual browsers. */
 describe('the isBrowserSupported functional tests', function () {
     let destroyDom;
@@ -21,27 +21,73 @@ describe('the isBrowserSupported functional tests', function () {
         expect(isBrowserSupported(featureConfig)).to.equal(true);
     });
 
-    it('should return false if the window.CustomEvent is missing', function () {
-        window.CustomEvent = undefined;
+    describe('window', function () {
+        it('should return false if CustomEvent is missing', function () {
+            window.CustomEvent = undefined;
 
-        expect(isBrowserSupported(featureConfig)).to.equal(false);
+            expect(isBrowserSupported(featureConfig)).to.equal(false);
+        });
+
+        it('should return false if CustomEvent is not a function', function () {
+            window.CustomEvent = {};
+            
+            expect(isBrowserSupported(featureConfig)).to.equal(false);
+        });
     });
 
-    it('should return false if the window.CustomEvent is not a function', function () {
-        window.CustomEvent = {};
-        
-        expect(isBrowserSupported(featureConfig)).to.equal(false);
+    describe('document', function () {
+        it('should return false if querySelector is missing', function () {
+            document.querySelector = undefined;
+            
+            expect(isBrowserSupported(featureConfig)).to.equal(false);
+        });
+
+        it('should return false if querySelector is not a function', function () {
+            document.querySelector = {};
+            
+            expect(isBrowserSupported(featureConfig)).to.equal(false);
+        });
     });
 
-    it('should return false if document.querySelector is missing', function () {
-        document.querySelector = undefined;
-        
-        expect(isBrowserSupported(featureConfig)).to.equal(false);
-    });
+    describe('HTMLElement', function () {
+        it('should return false if querySelector is missing', function () {
+            window.HTMLElement.prototype.querySelector = undefined;
+            
+            expect(isBrowserSupported(featureConfig)).to.equal(false);
+        });
 
-    it('should return false if document.querySelector is not a function', function () {
-        document.querySelector = {};
-        
-        expect(isBrowserSupported(featureConfig)).to.equal(false);
+        it('should return false if querySelector is not a function', function () {
+            window.HTMLElement.prototype.querySelector = {};
+            
+            expect(isBrowserSupported(featureConfig)).to.equal(false);
+        });
+
+        it('should return false if the foo property is missing', function () {
+            const fooConfig = [
+                {
+                    name: 'element',
+
+                    properties: [
+                        'foo'
+                    ]
+                }
+            ];
+
+            expect(isBrowserSupported(fooConfig)).to.equal(false);
+        });
+
+        it('should return false if the foo style property is missing', function () {
+            const fooConfig = [
+                {
+                    name: 'element',
+
+                    styles: [
+                        'foo'
+                    ]
+                }
+            ];
+
+            expect(isBrowserSupported(fooConfig)).to.equal(false);
+        });
     });
 });
