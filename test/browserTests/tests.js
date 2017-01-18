e3b97d6c53e2749b3d0210951e2f59a5fd1b33a4
James Wright <JAWright@Channel4.co.uk>
2017-01-18 17:35:39 +0000

    Chrome browser tests working

--- test/browserTests/tests.js
+++ test/browserTests/tests.js
@@ -1,7 +1,25 @@
 'use strict';
 
+const webdriver = require('selenium-webdriver');
+const { By, until, Builder: WebDriverBuilder } = webdriver;
+
 describe('the browser tests', function () {
     describe('Chrome', function () {
+        it('should be compatible with the specified feature config', function () {
+            const driver = new WebDriverBuilder()
+                .forBrowser('chrome')
+                .build();
 
+            driver.get('http://localhost:8080/testPage.html');
+
+            const resultElement = driver.findElement(By.id('result'));
+
+            return resultElement.getText()
+                .then(text => {
+                    expect(text).to.equal('true');
+                    
+                    return driver.quit();
+                });
+        });
     });
 });
