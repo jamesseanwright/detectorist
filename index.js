424851a72ce4a91cc521ee9795d18d5515d56d5a
James Wright <JAWright@Channel4.co.uk>
2017-01-18 13:41:55 +0000

    WIP implementation

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 'use strict';
 
+const specialMappings = {
+    get element() {
+        return document.createElement('div');
+    },
+
+    get window() {
+        return window;
+    }
+};
+
+function isGroupSupported(isSupported, group) {
+    const target = specialMappings[group.name] || window[group.name];
+    const areMethodsSupported = isTypeSupported(target, group.methods, 'function');
+
+    return areMethodsSupported;
+}
+
+function isTypeSupported(target, expectedFeatures, expectedType) {
+    if (!expectedFeatures) {
+        return true;
+    }
+
+    return expectedFeatures.reduce((isSupported, item) => {
+        const hasFeature = !!target[item];
+        const doesFeatureConformToType = expectedType ? typeof target[item] === expectedType : true;
+
+        return hasFeature && doesFeatureConformToType;
+    }, true);
+}
+
 module.exports = function isBrowserSupported(featureConfig) {
-    
+    return featureConfig.reduce(isGroupSupported, true);
 };
